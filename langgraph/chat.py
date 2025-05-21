ca7a60a32817aa9ec30ea67a20a9803951b6f57d
rama <hawarama344@gmail.com>
2025-05-21 22:56:27 +0530

    basic chatbot working

--- langgraph/chat.py
+++ langgraph/chat.py
@@ -14,23 +14,36 @@ load_dotenv()
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
-graph_builder = StateGraph(State)
-
-GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
-
-llm = init_chat_model("google_genai:gemini-2.0-flash")
-
 def chat_bot(state:State) -> State:
     return { "messages": [llm.invoke(state['messages'])]}
 
+def stream_graph_updates(input_user, graph ):
+    for event in graph.stream({"messages":[ {"role":"user", "content":input_user }]}):
+        for value in event.values():
+            print("Assistant:", value['messages'][0].content)
+
+graph_builder = StateGraph(State)
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+llm = init_chat_model("google_genai:gemini-2.0-flash")
 
 graph_builder.add_node("chat_bot", chat_bot)
 graph_builder.add_edge(START, "chat_bot")
 
 graph =graph_builder.compile()
 
-try:
-    display(Image(graph.get_graph().draw_mermaid_png()))
-except Exception:
-    # This requires some extra dependencies and is optional
-    pass
+while True:
+    try:
+        user_input = input("user:" )
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Bye Bye!")
+            break
+        stream_graph_updates(user_input, graph)
+    except Exception as error:
+        print("Didn't work: ", error)
+
+# try:
+#     display(Image(graph.get_graph().draw_mermaid_png()))
+# except Exception:
+#     # This requires some extra dependencies and is optional
+#     pass
+
